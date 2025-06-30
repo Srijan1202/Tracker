719e5130078b6676587fb081eebc58045e7e6ee6
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-06-30 15:02:53 +0530

    first change

---
+++ src/main/java/com/example/tracker/model/Task.java
@@ -0,0 +1,17 @@
+package com.example.tasktracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private boolean completed = false;
+}
